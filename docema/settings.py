44b0178ab0c0a5932a1b85ec3f055cbb20b91f2b
Jacob Mukiti <jacob97mukity@gmail.com>
2021-02-08 09:23:09 +0000

    Update

--- docema/settings.py
+++ docema/settings.py
@@ -7,9 +7,6 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hyj2g&-f)cbo4hb-dqq5*0&e9p-*mab74qdbllqs!0v)k4z(h1'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
